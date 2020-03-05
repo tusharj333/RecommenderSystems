dafa01b5f273704352a0a9890e34961a19e29751
tusharj333 <tusharj333@gmail.com>
2020-03-05 21:08:15 +0530

    changed base path of data

--- collaborative_filtering/user_user_CF.py
+++ collaborative_filtering/user_user_CF.py
@@ -3,14 +3,16 @@ from sortedcontainers import SortedList
 from tqdm import tqdm
 import numpy as np
 
-with open("./../data/user2movie.json", 'rb') as f:
+base_path = "./../data/"
+with open(base_path + "user2movie.json", 'rb') as f:
     user2movie = pickle.load(f)
-with open("./../data/movie2user.json", 'rb') as f:
+with open(base_path + "movie2user.json", 'rb') as f:
     movie2user = pickle.load(f)
-with open("./../data/usermovie2rating.json", 'rb') as f:
+with open(base_path + "usermovie2rating.json", 'rb') as f:
     usermovie2rating = pickle.load(f)
-with open("./../data/usermovie2rating_test.json", 'rb') as f:
+with open(base_path + "usermovie2rating_test.json", 'rb') as f:
     usermovie2rating_test = pickle.load(f)
+
 
 N = max(user2movie.keys()) + 1  # Number of Users
 M = max(max(movie2user.keys()), max(usermovie2rating.keys())[1]) + 1  # Number of Movies
